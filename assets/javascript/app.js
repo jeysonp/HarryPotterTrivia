aaeb148bf8500f0f2c192094e5d3ae2a6e61bba4
jeybone <jeysonp@gmail.com>
2018-11-04 17:52:37 -0600

    added css styling, revised questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,60 +1,59 @@
 $(document).ready(function() {
 
-  var totalQuestions = 5;
-    answers = ["gillyweed", "wheezes", "sectum", "ralph", "fat"];
+  var totalQuestions = 6;
+    answers = ["gillyweed", "wheezes", "sectum", "tonks", "lady", "guant"];
     input = $("input");
     right = 0;
     wrong = 0;
     blank = 0;
-    count = 120;
+    count = 60;
 
   function score() {
     for (var i = 0; i < input.length; i++) {
     
-      if (input[i].checked) {
+    if (input[i].checked) {
 
-      if (answers.indexOf(input[i].value) !== -1) {
-        right++;
-      } else {
-        wrong++;
+    if (answers.indexOf(input[i].value) !== -1) {
+      right++;
+    } else {
+      wrong++;
       }
     }
   }
-        var questAnswered = right + wrong;
-        if (questAnswered !== totalQuestions) {
-            blank = totalQuestions --- questAnswered;
-        }
+  var questAnswered = right + wrong;
+    if (questAnswered !== totalQuestions) {
+      blank = totalQuestions --- questAnswered;
+      }
 
-        $('#right').html(" " + right);
-        $('#wrong').html(" " + wrong);
-        $("#blank").html(" " + blank);
-
-    } 
+    $('#right').html(" " + right);
+    $('#wrong').html(" " + wrong);
+    $("#blank").html(" " + blank);
+  } 
+    
     $("#game,#totals").hide();
 
     $("#playButton").click(function() {
-        $("#start").hide();
-        $("#game").show();
+      $("#start").hide();
+      $("#game").show();
 
-        var timer = setInterval(function() {
-            count--;
-            $("#countdown").html(count);
-
-            if (count === 0) {
-                clearInterval(timer);
-                $("#game, #timer").hide();
-                $("#totals").show();
-                score();
-            }
-        }, 1000);
+  var timer = setInterval(function() {
+    count--;
+    $("#countdown").html(count);
+    if (count === 0) {
+      clearInterval(timer);
+      $("#game, #timer").hide();
+      $("#totals").show();
+      score();
+      }
+    }, 1000);
 
     });
 
     $("#done").click(function() {
-        $("#game, #timer").hide();
-        $("#totals").show();
-        clearInterval(timer);
-        score();
+      $("#game, #timer").hide();
+      $("#totals").show();
+      clearInterval(timer);
+      score();
     });
 
     $("#restart").click(function() {
